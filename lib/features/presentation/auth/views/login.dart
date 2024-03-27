da5a5847680b8587bf6520622e11b2f82ea886ee
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-03-27 07:08:50 +0200

    Add LoginView component for authentication feature
    Create a new LoginView component with necessary widgets for the authentication
    feature. This component includes email and password fields, buttons for login
    and registration, and a forgot password option.

---
+++ lib/features/presentation/auth/views/login.dart
@@ -0,0 +1,81 @@
+import 'package:chatie/features/presentation/auth/views/widgets/app_logo.dart';
+import 'package:chatie/features/presentation/auth/views/widgets/text_fields.dart';
+import 'package:flutter/material.dart';
+
+import 'widgets/button.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const AppLogo(),
+            Text(
+              "Welcome Back! 👋",
+              style: Theme.of(context).textTheme.headlineMedium,
+              // style: context.hm!.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            EmailField(
+              validator: (data) {
+                if (data == null || data.isEmpty) {
+                  return 'Please enter your email';
+                } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                    .hasMatch(data)) {
+                  return 'Invalid email format.';
+                }
+                return null;
+              },
+              // In validation, returning null indicates that the input is valid
+              onchanged: (data) {},
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            PasswordField(
+              validator: (data) {
+                if (data == null || data.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              // In validation, returning null indicates that the input is valid
+              onchanged: (data) {},
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Forgot Password?",
+                  ),
+                ),
+              ],
+            ),
+            FillButton(
+              onPressed: () {},
+              text: 'Login',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            OutLineButton(
+              onPressed: () {},
+              text: "Register",
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
