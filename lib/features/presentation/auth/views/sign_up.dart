7c97bc46f2c96a2cd22484591d9b7ad276add301
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-03-27 21:36:29 +0200

    Add sign up screen with form fields and validation logic
    Implement the sign up screen with form fields for first name, last name, email, and password. Include validation logic for each field to ensure data integrity.

---
+++ lib/features/presentation/auth/views/sign_up.dart
@@ -0,0 +1,101 @@
+import 'package:chatie/features/presentation/auth/views/widgets/button.dart';
+import 'package:chatie/features/presentation/auth/views/widgets/text_fields.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        children: [
+          const SizedBox(height: 16),
+          Text(
+            "Create an Account",
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "First name",
+          ),
+          const SizedBox(height: 8),
+          TextForm(
+            icon: Icons.person,
+            hinttext: "First name",
+            validator: (data) {
+              if (data == null || data.isEmpty) {
+                return 'Please enter your first name ';
+              } else if (data.length > 8) {
+                return 'First name must be at most 8 characters';
+              }
+              return null;
+            },
+            onchanged: (data) {},
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Last name",
+          ),
+          const SizedBox(height: 8),
+          TextForm(
+            icon: Icons.person,
+            hinttext: "Last name",
+            validator: (data) {
+              if (data == null || data.isEmpty) {
+                return 'Please enter your last name';
+              } else if (data.length > 8) {
+                return 'Last name must be at most 8 characters';
+              }
+              return null;
+            },
+            onchanged: (data) {},
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Email",
+          ),
+          const SizedBox(height: 8),
+          TextForm(
+            icon: Icons.email,
+            hinttext: "Email",
+            validator: (data) {
+              if (data == null || data.isEmpty) {
+                return 'Please enter your email';
+              } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                  .hasMatch(data)) {
+                return 'Invalid email format.';
+              }
+              return null;
+            },
+            onchanged: (data) {},
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Password",
+          ),
+          const SizedBox(height: 8),
+          PasswordField(
+            validator: (data) {
+              if (data == null || data.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+            onchanged: (data) {},
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          FillButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              text: "Sign Up")
+        ],
+      ),
+    );
+  }
+}
