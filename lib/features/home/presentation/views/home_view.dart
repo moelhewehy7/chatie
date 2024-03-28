34b9043775e4e153c4bb3cc58659602a00dbc59f
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-03-28 21:32:07 +0200

    update navigation icons and modfiy nav bar index

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -8,48 +9,61 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  int currentindex = 0;
-  List<Widget> scrrens = [
-    Container(
-      color: Colors.blue,
-    ),
-    Container(
-      color: Colors.red,
-    ),
-    Container(
-      color: Colors.green,
-    ),
-    Container(
-      color: Colors.pinkAccent,
-    ),
-  ];
+  int currentIndex = 0;
+  PageController pageController = PageController();
+  //is used to control the PageView and enable programmatic page navigation.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: scrrens[currentindex],
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        //updates the currentindex variable to reflect the selectedIndex property
+        children: [
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.pinkAccent,
+          ),
+        ],
+      ),
       bottomNavigationBar: NavigationBar(
-        selectedIndex: currentindex,
+        elevation: 0,
+        selectedIndex: currentIndex,
         onDestinationSelected: (value) {
-          currentindex = value;
-          setState(() {});
+          setState(() {
+            currentIndex = value;
+          });
+          pageController.jumpToPage(value);
         },
         destinations: const [
           NavigationDestination(
-              selectedIcon: Icon(Icons.message),
-              icon: Icon(Icons.message_outlined),
-              label: "Message"),
+              selectedIcon: Icon(IconlyBold.chat),
+              icon: Icon(IconlyLight.chat),
+              label: "Chat"),
           NavigationDestination(
-              selectedIcon: Icon(Icons.group),
-              icon: Icon(Icons.group_outlined),
+              selectedIcon: Icon(Icons.groups),
+              icon: Icon(Icons.groups_outlined),
               label: "Groups"),
           NavigationDestination(
               selectedIcon: Icon(Icons.contacts),
               icon: Icon(Icons.contacts_outlined),
               label: "Contacts"),
           NavigationDestination(
-              selectedIcon: Icon(Icons.settings),
-              icon: Icon(Icons.settings_outlined),
+              selectedIcon: Icon(IconlyBold.setting),
+              icon: Icon(IconlyLight.setting),
               label: "Settings")
         ],
       ),
