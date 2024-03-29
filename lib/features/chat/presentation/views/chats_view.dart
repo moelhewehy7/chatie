3e32bc444de10e4a02c25a5d4d69bfb83d4ef40a
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-03-29 05:24:36 +0200

    Add friend email input field in ChatView
    Import TextForm widget and show input field for friend email in ChatView's
    floating action button onPressed event. This allows users to enter their
    friend's email address.

--- lib/features/chat/presentation/views/chats_view.dart
+++ lib/features/chat/presentation/views/chats_view.dart
@@ -1,16 +1,43 @@
+import 'package:chatie/features/auth/presentation/views/widgets/text_fields.dart';
 import 'package:chatie/features/chat/presentation/views/widgets/chat_card.dart';
-import 'package:chatie/features/chat/presentation/views/widgets/chats_view_body.dart';
 import 'package:flutter/material.dart';
-import 'package:iconly/iconly.dart';
 
-class ChatView extends StatelessWidget {
+class ChatView extends StatefulWidget {
   const ChatView({super.key});
 
+  @override
+  State<ChatView> createState() => _ChatViewState();
+}
+
+class _ChatViewState extends State<ChatView> {
+  TextEditingController emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showBottomSheet(
+              context: context,
+              builder: (context) {
+                return Container(
+                  padding: EdgeInsets.all(24),
+                  child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Enter friend email",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        TextForm(
+                          controller: emailController,
+                          hinttext: "Email",
+                          icon: Icons.email,
+                        )
+                      ]),
+                );
+              });
+        },
         child: Icon(
           Icons.maps_ugc,
           size: 27,
