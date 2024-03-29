a819f0b776b256899635953949dec945c1f045e2
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-03-29 06:01:20 +0200

    Add button and icon imports, update bottom sheet UI

--- lib/features/chat/presentation/views/chats_view.dart
+++ lib/features/chat/presentation/views/chats_view.dart
@@ -1,10 +1,11 @@
+import 'package:chatie/features/auth/presentation/views/widgets/button.dart';
 import 'package:chatie/features/auth/presentation/views/widgets/text_fields.dart';
 import 'package:chatie/features/chat/presentation/views/widgets/chat_card.dart';
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
 
 class ChatView extends StatefulWidget {
   const ChatView({super.key});
-
   @override
   State<ChatView> createState() => _ChatViewState();
 }
@@ -17,26 +18,45 @@ class _ChatViewState extends State<ChatView> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showBottomSheet(
-              context: context,
-              builder: (context) {
-                return Container(
-                  padding: const EdgeInsets.all(24),
-                  child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 32, vertical: 16), // EdgeInsets.all(24),
+                child: Column(mainAxisSize: MainAxisSize.min, children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: Row(
                       children: [
                         Text(
                           "Enter friend email",
                           style: Theme.of(context).textTheme.titleLarge,
                         ),
-                        TextForm(
-                          controller: emailController,
-                          hinttext: "Email",
-                          icon: Icons.email,
-                        )
-                      ]),
-                );
-              });
+                        Spacer(),
+                        IconButton.filled(
+                            onPressed: () {}, icon: Icon(IconlyBold.scan))
+                      ],
+                    ),
+                  ),
+                  TextForm(
+                    controller: emailController,
+                    hinttext: "Email",
+                    icon: Icons.email,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12),
+                        child:
+                            FillButton(onPressed: () {}, text: "Create chat"),
+                      ),
+                    ],
+                  )
+                ]),
+              );
+            },
+          );
         },
         child: const Icon(
           Icons.maps_ugc,
@@ -51,7 +71,7 @@ class _ChatViewState extends State<ChatView> {
           ),
           SliverList(
               delegate: SliverChildBuilderDelegate(
-            childCount: 16,
+            childCount: 3,
             (context, index) {
               return const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16),
