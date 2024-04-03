ec677c818ab1735ea98e990979734b081d346ebe
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-04-03 05:23:19 +0200

    Refactor ContactsView widget in contacts_view.dart
    Remove unused imports and update widget constructors to use const where applicable.

--- lib/features/contacts/presentation/views/contacts_view.dart
+++ lib/features/contacts/presentation/views/contacts_view.dart
@@ -1,8 +1,6 @@
-import 'package:chatie/features/auth/presentation/views/widgets/forgot_password.dart';
 import 'package:chatie/features/auth/presentation/views/widgets/text_fields.dart';
 import 'package:chatie/features/chats/presentation/views/widgets/show_bottom_sheet.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:iconly/iconly.dart';
 
 class ContactsView extends StatefulWidget {
@@ -35,43 +33,47 @@ class _ContactsViewState extends State<ContactsView> {
                 controller: contactController,
                 hinttext: "Search by name",
                 icon: Icons.search,
-                validator: (value) {},
-              )
-            : Text("Contacts"),
+                validator: (data) {
+                  if (data == null || data.isEmpty) {
+                    return 'Field is empty';
+                  }
+                  return null;
+                })
+            : const Text("Contacts"),
         actions: [
           searching
               ? Padding(
-                  padding: EdgeInsets.only(right: 12),
+                  padding: const EdgeInsets.only(right: 12),
                   child: TextButton(
                       onPressed: () {
                         searching = false;
                         setState(() {});
                       },
-                      child: Text(
+                      child: const Text(
                         "Cancel",
                       )),
                 )
               : IconButton(
-                  padding: EdgeInsets.only(right: 3),
+                  padding: const EdgeInsets.only(right: 3),
                   onPressed: () {
                     searching = true;
                     setState(() {});
                   },
-                  icon: Icon(
+                  icon: const Icon(
                     Icons.person_search,
                     size: 26,
                   ))
         ],
       ),
       body: ListView.builder(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
         itemCount: 6,
         itemBuilder: (BuildContext context, int index) {
           return Card(
             child: ListTile(
-              title: Text("Name"),
-              trailing:
-                  IconButton(onPressed: () {}, icon: Icon(IconlyBold.chat)),
+              title: const Text("Name"),
+              trailing: IconButton(
+                  onPressed: () {}, icon: const Icon(IconlyBold.chat)),
             ),
           );
         },
