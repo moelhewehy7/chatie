1f9ef0d43311820413a8c4271e07674e5cf08a2a
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-04-03 05:23:10 +0200

    Refactor settings view layout and functionality
    Update settings view to include profile, themes, dark mode, and signout options.
    Improve UI layout and functionality for better user experience.

---
+++ lib/features/settings/presentation/views/settings_view.dart
@@ -0,0 +1,64 @@
+import 'package:chatie/features/settings/presentation/views/widgets/profile_view.dart';
+import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              minVerticalPadding: 40,
+              leading: const CircleAvatar(
+                radius: 40,
+              ),
+              title: const Text("Name"),
+              trailing: IconButton(
+                  onPressed: () {}, icon: const Icon(IconlyBold.scan)),
+            ),
+            Card(
+                child: ListTile(
+              leading: const Icon(IconlyBold.profile),
+              title: const Text("Profile"),
+              trailing: IconButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const ProfileView();
+                    }));
+                  },
+                  icon: const Icon(IconlyLight.arrow_right)),
+            )),
+            const Card(
+                child: ListTile(
+              leading: Icon(IconlyBold.category),
+              title: Text("Themes"),
+            )),
+            Card(
+                child: ListTile(
+              leading: const Icon(Icons.dark_mode),
+              title: const Text("Dark mode"),
+              trailing: Switch(value: true, onChanged: (value) {}),
+            )),
+            Card(
+                child: ListTile(
+              title: const Text("Signout"),
+              trailing: IconButton(
+                  onPressed: () {}, icon: const Icon(IconlyLight.logout)),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
