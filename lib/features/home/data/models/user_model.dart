dc2ed46d872287ab2cc64b7e410c5d9f73678a6e
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-04-18 18:13:21 +0200

    Add UserModel class with constructor and factory method
    Added UserModel class with constructor and factory method for decoding JSON data
    automatically. This class includes properties for id, name, bio, email, lastSeen,
    isOnline, createdAt, profilePic, and pushToken.

---
+++ lib/features/home/data/models/user_model.dart
@@ -0,0 +1,40 @@
+class UserModel {
+  final String? id;
+  final String? name;
+  final String? bio;
+  final String? email;
+  final bool? lastSeen;
+  final bool? isOnline;
+  final String? createdAt;
+  final String? profilePic;
+  final String? pushToken;
+
+  UserModel(
+      {required this.id,
+      required this.name,
+      required this.bio,
+      required this.email,
+      required this.lastSeen,
+      required this.isOnline,
+      required this.createdAt,
+      required this.profilePic,
+      required this.pushToken});
+  factory UserModel.fromjson(jsonData) {
+    // the data is automatically decoded ,there's no need to manually decode it
+    return UserModel(
+        id: jsonData['id'],
+        name: jsonData['name'],
+        bio: jsonData['bio'],
+        email: jsonData['email'],
+        lastSeen: jsonData['last_seen'],
+        isOnline: jsonData['is_online'],
+        createdAt: jsonData['created_at'],
+        profilePic: jsonData['profile_pic'],
+        pushToken: jsonData['push_token']);
+  }
+}
+//When you fetch data from Firebase, you receive a JSON representation of the data.
+// then use your fromJson factory method to convert this JSON data into a UserModel object
+
+// initializing your UserModel object when fetching data ensures 
+//that your model reflects the actual data in your database
