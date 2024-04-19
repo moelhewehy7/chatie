1749c71e836b98b71ba3278b376059f30eeeb46d
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-04-19 20:23:48 +0200

    Refactor showbottomsheet function - Updated the function to use a Form widget for better form handling. - Replaced hardcoded function call with a parameter for button action. - Added a key parameter for better widget identification.

--- lib/features/chats/presentation/views/widgets/show_bottom_sheet.dart
+++ lib/features/chats/presentation/views/widgets/show_bottom_sheet.dart
@@ -1,50 +1,57 @@
 import 'package:chatie/features/auth/presentation/views/widgets/button.dart';
 import 'package:chatie/features/auth/presentation/views/widgets/text_fields.dart';
-import 'package:chatie/features/chats/data/create_room.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:iconly/iconly.dart';
 
 PersistentBottomSheetController showbottomsheet(
-    BuildContext context, TextEditingController emailController, String text) {
+    {required BuildContext context,
+    required TextEditingController emailController,
+    required String? Function(String?)? validator,
+    required void Function() onpressed,
+    required String buttonName,
+    required Key key}) {
   return showBottomSheet(
     context: context,
     builder: (BuildContext context) {
       return Container(
         padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 12),
-            child: Row(
-              children: [
-                Text(
-                  "Enter friend email",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const Spacer(),
-                IconButton.filled(
-                    onPressed: () {}, icon: const Icon(IconlyBold.scan))
-              ],
-            ),
-          ),
-          TextForm(
-            controller: emailController,
-            hinttext: "Email",
-            icon: Icons.email,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 12),
-                child: FillButton(
-                    onPressed: () {
-                      Room().create(email: emailController.text);
-                    },
-                    child: Text(text)),
+        child: Form(
+          key: key,
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 12),
+              child: Row(
+                children: [
+                  Text(
+                    "Enter friend email",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const Spacer(),
+                  IconButton.filled(
+                      onPressed: () {}, icon: const Icon(IconlyBold.scan))
+                ],
               ),
-            ],
-          )
-        ]),
+            ),
+            TextForm(
+              controller: emailController,
+              validator: validator,
+              hinttext: "Email",
+              icon: Icons.email,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.only(top: 12),
+                    child: FillButton(
+                      onPressed: onpressed,
+                      child: Text(buttonName),
+                    )),
+              ],
+            )
+          ]),
+        ),
       );
     },
   );
