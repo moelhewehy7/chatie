6699e5b092d6c9aeee4974ce078b473d1dea14ff
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-04-21 00:19:15 +0200

    Update chat card widget to display user information and messages dynamically
    Refactored the ChatCard widget to fetch user information from Firestore based on
    the chat room members and display it dynamically. Also updated the message
    display logic to show the last message or user bio if no message is available.

--- lib/features/chats/presentation/views/widgets/chat_card.dart
+++ lib/features/chats/presentation/views/widgets/chat_card.dart
@@ -1,5 +1,8 @@
 import 'package:chatie/features/chats/data/models/chat_room_model.dart';
 import 'package:chatie/features/chats/presentation/views/widgets/chat_view_body.dart';
+import 'package:chatie/features/home/data/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -13,30 +16,53 @@ class ChatCard extends StatelessWidget {
   final ChatRoomModel chatRoom;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        splashColor: Colors.transparent,
-        onTap: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const ChatViewBody()));
-        },
-        leading: const CircleAvatar(),
-        title: Text(chatRoom.id ?? 'Friend'),
-        subtitle: const Text("Last message"),
-        trailing: Column(
-          children: [
-            Text(DateFormat('h:mm a')
-                .format(DateTime.parse(chatRoom.createdAt ?? ""))),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Badge(
-                backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-                smallSize: 15,
+    String userEmail = chatRoom.members!
+        .where((element) => element != FirebaseAuth.instance.currentUser!.email)
+        .first; // to get useremail from memebers list
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("users")
+            .doc(userEmail)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserModel userModel = UserModel.fromjson(snapshot.data!.data());
+            return Card(
+              child: ListTile(
+                splashColor: Colors.transparent,
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ChatViewBody(
+                                userName: userModel.firstName!,
+                                roomId: chatRoom.id!,
+                              )));
+                },
+                leading: const CircleAvatar(),
+                title: Text(userModel.firstName ?? 'Friend'),
+                subtitle: Text(chatRoom.lastMessage != "lastMessage"
+                    ? chatRoom.lastMessage!
+                    : userModel.bio!),
+                trailing: Column(
+                  children: [
+                    Text(DateFormat('h:mm a')
+                        .format(DateTime.parse(chatRoom.createdAt ?? ""))),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Badge(
+                        backgroundColor:
+                            Theme.of(context).colorScheme.primaryContainer,
+                        smallSize: 15,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            );
+          } else {
+            return const SizedBox();
+          }
+        });
   }
 }
