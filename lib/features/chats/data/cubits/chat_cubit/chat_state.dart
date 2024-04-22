b3d371fb4db70ac49052c261cb327a180e33bcde
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-04-23 01:12:54 +0200

    Add message fetching functionality to ChatCubit
    Add functionality to fetch messages from a specific room in ChatCubit.
    Update ChatState to include ChatNew state and modify ChatSuccess state to include messages.

--- lib/features/chats/data/cubits/chat_cubit/chat_state.dart
+++ lib/features/chats/data/cubits/chat_cubit/chat_state.dart
@@ -5,4 +5,9 @@ sealed class ChatState {}
 
 final class ChatInitial extends ChatState {}
 
-final class ChatSuccess extends ChatState {}
+final class ChatNew extends ChatState {}
+
+final class ChatSuccess extends ChatState {
+  List<MessageModel> messages;
+  ChatSuccess({required this.messages});
+}
