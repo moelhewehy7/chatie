a982a4a2acab8b167dc99bf6c85261d62cd8e7f2
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-04-24 20:28:02 +0200

    Update ChatBuble widget with image handling and responsive design

--- lib/features/chats/presentation/views/widgets/chat_bubles.dart
+++ lib/features/chats/presentation/views/widgets/chat_bubles.dart
@@ -1,12 +1,16 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chatie/features/chats/data/models/message_model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ChatBuble extends StatelessWidget {
   const ChatBuble({super.key, required this.messageModel});
   final MessageModel messageModel;
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.sizeOf(context).height;
+    double width = MediaQuery.sizeOf(context).width;
     return Card(
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
@@ -19,20 +23,51 @@ class ChatBuble extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.only(top: 8, bottom: 8, left: 10, right: 5),
           child: Container(
-            constraints:
-                BoxConstraints(maxWidth: MediaQuery.sizeOf(context).width / 2),
+            constraints: BoxConstraints(maxWidth: width / 2),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(right: 5),
-                  child: Text(
-                    messageModel.message!,
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleMedium!
-                        .copyWith(color: Theme.of(context).colorScheme.surface),
-                  ),
+                  padding: const EdgeInsets.only(right: 5, top: 5),
+                  child: messageModel.type == "image"
+                      ? Container(
+                          height: height * 0.35,
+                          width: width * 0.5,
+                          padding: EdgeInsets.only(bottom: 3),
+                          child: SizedBox(
+                            height: height * 0.35,
+                            width: width * 0.5,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: CachedNetworkImage(
+                                imageUrl: messageModel.message!,
+                                placeholder: (context, url) =>
+                                    Shimmer.fromColors(
+                                  baseColor: const Color(0xFFE0E0E0),
+                                  highlightColor: const Color(0xFFF5F5F5),
+                                  child: Container(
+                                    width: double.infinity,
+                                    height:
+                                        200.0, // Adjust the height as needed
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                errorWidget: (context, url, error) => SizedBox(
+                                    height: height * 0.25,
+                                    width: width * 0.2,
+                                    child: Center(child: Icon(Icons.error))),
+                              ),
+                            ),
+                          ),
+                        )
+                      : Text(
+                          messageModel.message!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium!
+                              .copyWith(
+                                  color: Theme.of(context).colorScheme.surface),
+                        ),
                 ),
                 Row(
                   mainAxisSize: MainAxisSize.min,
