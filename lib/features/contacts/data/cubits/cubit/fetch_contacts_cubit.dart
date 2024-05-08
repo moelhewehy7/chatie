d580cdf24a603fa1096ae5c25791fadaa17dc621
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-05-08 18:09:15 +0300

    Add import for 'flutter/widgets.dart' in fetch_contacts_cubit.dart
    Import 'flutter/widgets.dart' to resolve missing dependency in the file.

--- lib/features/contacts/data/cubits/cubit/fetch_contacts_cubit.dart
+++ lib/features/contacts/data/cubits/cubit/fetch_contacts_cubit.dart
@@ -4,6 +4,7 @@ import 'package:chatie/features/home/data/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'fetch_contacts_state.dart';
@@ -14,13 +15,12 @@ class FetchContactsCubit extends Cubit<FetchContactsState> {
   Future<void> fetchContacts() async {
     emit(FetchContactsLoading());
     try {
-      FirebaseFirestore.instance
+      _subscription = FirebaseFirestore.instance
           .collection("users")
           .doc(FirebaseAuth.instance.currentUser!.email)
           .snapshots()
           .listen((event) {
         List myUsers = event.data()?["myUsers"]; //to get the list of contacts
-
         if (myUsers.isNotEmpty) {
           //search for each contact and get each doc of each one
           FirebaseFirestore.instance
@@ -30,8 +30,8 @@ class FetchContactsCubit extends Cubit<FetchContactsState> {
               .listen((event) {
             //return query snapshot of each user and then convet it to list of user model
             List<UserModel> userList = [];
-            for (var element in event.docs) {
-              userList.add(UserModel.fromjson(element));
+            for (var doc in event.docs) {
+              userList.add(UserModel.fromjson(doc));
             }
             emit(FetchContactsSuccess(users: userList));
           });
@@ -54,3 +54,15 @@ class FetchContactsCubit extends Cubit<FetchContactsState> {
 //get list of contacts from the current user
 // check if list is empty or not
 // If the list is not empty, it queries Firestore for user details based on the "myUsers" list.
+
+//
+// The "Bad state: Cannot add new events after calling close" error occurs
+// when you attempt to emit a new state from a Cubit after it has been closed.
+// In your case, the error likely occurred because the close() method was 
+//called implicitly by the framework, possibly when the widget associated with the Cubit was disposed.
+// Even though you didn't explicitly call close() on the Cubit,
+// it's possible that the framework did so under certain circumstances, 
+//such as when the widget that created the Cubit was disposed. When the Cubit is closed,
+// any subsequent attempts to emit new states will result in the "Bad state" error.
+// To prevent this error, ensure that you're not trying to emit new states from a Cubit after
+// its associated widget has been disposed or after the Cubit has been explicitly closed.
