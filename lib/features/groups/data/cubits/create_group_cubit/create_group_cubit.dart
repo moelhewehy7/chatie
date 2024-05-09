f23ea9ad9a497a42afa8c0789db57a670f3c3176
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-05-10 00:43:58 +0300

    Add CreateGroupCubit and CreateGroupState classes
    Added CreateGroupCubit class with createGroup method for creating a new group
    and added CreateGroupState classes for managing the state of group creation process.

---
+++ lib/features/groups/data/cubits/create_group_cubit/create_group_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uuid/uuid.dart';
+
+part 'create_group_state.dart';
+
+class CreateGroupCubit extends Cubit<CreateGroupState> {
+  CreateGroupCubit() : super(CreateGroupInitial());
+
+  Future createGroup({required String groupName, required List members}) async {
+    String groubId = const Uuid().v1();
+    String myEmail = FirebaseAuth.instance.currentUser!.email!;
+    members.add(myEmail);
+    try {
+      emit(CreateGroupLoading());
+      await FirebaseFirestore.instance.collection("groups").doc().set({
+        "id": groubId,
+        "name": groupName,
+        "image": "",
+        "admins": [myEmail],
+        "lastMessage": "",
+        "lastMessageTime": DateTime.now().millisecondsSinceEpoch.toString(),
+        "members": members,
+        "createdAt": DateTime.now().millisecondsSinceEpoch.toString()
+      });
+      emit(CreateGroupSuccess());
+    } catch (e) {
+      emit(CreateGroupFailure(
+          errMessage: "Oops, something went wrong, please try again later."));
+    }
+  }
+}
