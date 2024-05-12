9246cbfb7649a3f57c4e6a0d7408ab0ed201b659
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-05-13 02:32:58 +0300

    Add initial value to TextForm and GroupMembersView
    - Added initial value parameter to TextForm widget for setting initial value
      in TextFormField.
    - Added initial value parameter to GroupMembersView widget for setting
      initial value in text field controller.

--- lib/features/groups/presentation/views/widgets/group_members_view.dart
+++ lib/features/groups/presentation/views/widgets/group_members_view.dart
@@ -1,19 +1,28 @@
 import 'package:chatie/features/auth/presentation/views/widgets/text_fields.dart';
+import 'package:chatie/features/groups/data/models/group_model.dart';
 import 'package:chatie/features/groups/presentation/views/widgets/group_add_members.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
 
 class GroupMembersView extends StatefulWidget {
-  const GroupMembersView({super.key});
-
+  const GroupMembersView(
+      {super.key, required this.initialValue, required this.groupModel});
+  final String? initialValue;
+  final GroupModel groupModel;
   @override
   State<GroupMembersView> createState() => _GroupMembersViewState();
 }
 
 class _GroupMembersViewState extends State<GroupMembersView> {
-  TextEditingController groupcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    TextEditingController groupcontroller =
+        TextEditingController(text: widget.initialValue);
+
+    bool isadmin = widget.groupModel.admins!
+        .contains(FirebaseAuth.instance.currentUser!.email);
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
@@ -70,26 +79,41 @@ class _GroupMembersViewState extends State<GroupMembersView> {
             ),
             const Divider(),
             Expanded(
-              child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text("Member $index"),
-                    subtitle: const Text("Admin"),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.admin_panel_settings)),
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.person_remove))
-                      ],
-                    ),
-                  );
-                },
-              ),
+              child: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection("users")
+                      .where("Email", isEqualTo: widget.groupModel.members)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: widget.groupModel.members!.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            title: Text("Member $index"),
+                            subtitle: const Text("Admin"),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: isadmin
+                                        ? const Icon(Icons.admin_panel_settings)
+                                        : SizedBox()),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: isadmin
+                                        ? const Icon(Icons.person_remove)
+                                        : SizedBox()),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return SizedBox();
+                    }
+                  }),
             ),
           ],
         ),
