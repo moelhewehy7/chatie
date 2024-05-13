fe91ef2736348cd86650155e514e3b4e7af2b6fb
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-05-13 23:34:02 +0300

    Refactor group members view and add members functionality
    - Refactored the group members view to improve UI and functionality
    - Added the ability to add and remove members in a group
    - Updated Firestore methods for editing and removing group members

--- lib/features/groups/presentation/views/widgets/group_members_view.dart
+++ lib/features/groups/presentation/views/widgets/group_members_view.dart
@@ -1,3 +1,4 @@
+import 'package:chatie/core/helper.dart';
 import 'package:chatie/features/auth/presentation/views/widgets/text_fields.dart';
 import 'package:chatie/features/groups/data/models/group_model.dart';
 import 'package:chatie/features/groups/presentation/views/widgets/group_add_members.dart';
@@ -22,34 +23,28 @@ class _GroupMembersViewState extends State<GroupMembersView> {
     TextEditingController groupcontroller =
         TextEditingController(text: widget.initialValue);
 
-    bool isadmin = widget.groupModel.admins!
+    bool isAdmin = widget.groupModel.admins!
         .contains(FirebaseAuth.instance.currentUser!.email);
+    String myEmail = FirebaseAuth.instance.currentUser!.email!;
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () async {
+          await editGroup(
+            groupId: widget.groupModel.id!,
+            name: groupcontroller.text,
+          ).then((value) => Navigator.of(context).pop());
+        },
         label: const Text("Done"),
         icon: const Icon(Icons.check_circle),
       ),
       appBar: AppBar(
         title: const Text("Group info"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const GroupAddMembers()));
-              },
-              icon: const Icon(Icons.person_add_alt_1))
-        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: 16,
-        ),
-        child: Column(
-          children: [
-            Row(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Stack(
@@ -75,62 +70,107 @@ class _GroupMembersViewState extends State<GroupMembersView> {
                 )
               ],
             ),
-            const SizedBox(
-              height: 16,
-            ),
-            const Divider(),
-            Expanded(
-              child: StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection("users")
-                      .where("Email", whereIn: widget.groupModel.members)
-                      .snapshots(), //
-                  // In summary, equal to is used for single condition filters, while whereIn is used for filters with multiple values.
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      List<UserModel> members = snapshot.data!.docs
-                          .map((e) => UserModel.fromjson(e))
-                          .toList();
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () async {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => GroupAddMembers(
+                        groupModel: widget.groupModel,
+                      )));
+            },
+            contentPadding: const EdgeInsets.only(left: 19),
+            leading: IconButton.filledTonal(
+                padding: const EdgeInsets.all(9),
+                onPressed: () async {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => GroupAddMembers(
+                            groupModel: widget.groupModel,
+                          )));
+                },
+                icon: const Icon(
+                  Icons.person_add_alt_1,
+                  size: 22,
+                )),
+            title: const Text("Add members"),
+          ),
+          Expanded(
+            child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("users")
+                    .where("Email",
+                        whereIn: widget.groupModel
+                            .members) //etrieve documents  where the "Email" field matches any of the values
+                    .snapshots(), //
+                // In summary, equal to is used for single condition filters, while whereIn is used for filters with multiple values.
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<UserModel> members = snapshot.data!.docs
+                        .map((e) => UserModel.fromjson(e))
+                        .toList();
 
-                      return ListView.builder(
-                        itemCount: members.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          bool admin = widget.groupModel.admins!
-                              .contains(members[index].email);
-                          return ListTile(
-                            title: Text(
-                                "${members[index].firstName!} ${members[index].lastName!}"),
-                            subtitle: admin
-                                ? Text(
-                                    "Admin",
-                                    style: TextStyle(color: Colors.blue),
-                                  )
-                                : const Text("Member"),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: isadmin
-                                        ? const Icon(Icons.admin_panel_settings)
-                                        : SizedBox()),
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: isadmin
-                                        ? const Icon(Icons.person_remove)
-                                        : SizedBox()),
-                              ],
-                            ),
-                          );
-                        },
-                      );
-                    } else {
-                      return SizedBox();
-                    }
-                  }),
-            ),
-          ],
-        ),
+                    return ListView.builder(
+                      itemCount: members.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        bool admin = widget.groupModel.admins!
+                            .contains(members[index].email);
+                        return ListTile(
+                          onTap: () {},
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 24),
+                          leading: CircleAvatar(
+                            child: Text(members[index]
+                                .firstName!
+                                .toUpperCase()
+                                .characters
+                                .first),
+                          ),
+                          title: Text(
+                              "${members[index].firstName!} ${members[index].lastName!}"),
+                          subtitle: admin
+                              ? const Text(
+                                  "Admin",
+                                  style: TextStyle(color: Colors.blue),
+                                )
+                              : const Text("Member"),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: isAdmin
+                                      ? const Icon(Icons.admin_panel_settings)
+                                      : const SizedBox()),
+                              isAdmin && myEmail == members[index].email
+                                  ? const SizedBox()
+                                  : IconButton(
+                                      onPressed: () async {
+                                        await removeMember(
+                                                groupId: widget.groupModel.id!,
+                                                member: members[index].email!)
+                                            .then((value) => setState(() {
+                                                  widget.groupModel.members!
+                                                      .remove(members[index]
+                                                          .email!);
+                                                }));
+                                      },
+                                      icon: isAdmin
+                                          ? const Icon(Icons.person_remove)
+                                          : const SizedBox())
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const SizedBox();
+                  }
+                }),
+          ),
+        ],
       ),
     );
   }
