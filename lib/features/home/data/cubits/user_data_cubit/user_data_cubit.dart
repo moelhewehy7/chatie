2917d26c97631baa0a6cea2ba62ff2c79fa6e483
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-05-17 22:27:40 +0300

    Refactor UserDataCubit to update user data correctly
    This commit refactors the UserDataCubit to correctly update user data when the user logs in with a new account. It also adds debug print statements for better debugging.

--- lib/features/home/data/cubits/user_data_cubit/user_data_cubit.dart
+++ lib/features/home/data/cubits/user_data_cubit/user_data_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:chatie/features/home/data/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,13 +10,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'user_data_state.dart';
 
 class UserDataCubit extends Cubit<UserDataState> {
-  UserDataCubit() : super(UserDataInitial());
-  final myEmail = FirebaseAuth.instance.currentUser!.email;
+  UserDataCubit() : super(UserDataInitial()) {
+    getUserData();
+  }
+
   Future<void> updateUserData(
       {required String firstName,
       required String lastName,
       required String bio,
       required String imageUrl}) async {
+    final myEmail = FirebaseAuth.instance.currentUser!.email;
+    print("${myEmail}this is my email");
     emit(UserDataLoading());
     await Future.delayed(const Duration(seconds: 2));
     await FirebaseFirestore.instance.collection("users").doc(myEmail).update({
@@ -26,12 +32,29 @@ class UserDataCubit extends Cubit<UserDataState> {
   }
 
   getUserData() {
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(myEmail)
-        .snapshots()
-        .listen((event) {
-      emit(UserDataSuccess(userModel: UserModel.fromjson(event)));
+    FirebaseAuth.instance.userChanges().listen((user) {
+      if (user != null) {
+        //// the user's data should update correctly when they log in with a new account.
+        FirebaseFirestore.instance
+            .collection("users")
+            .doc(user.email)
+            .snapshots()
+            .listen((event) {
+          debugPrint("${user.email}this is my user.email");
+          emit(UserDataSuccess(userModel: UserModel.fromjson(event)));
+        });
+      }
     });
   }
+
+  // void resetData() {
+  //   eventData.clear();
+  //   emit(UserDataInitial());
+  // }
+
+  // @override
+  // Future<void> close() {
+  //   _subscription?.cancel();
+  //   return super.close();
+  // }
 }
