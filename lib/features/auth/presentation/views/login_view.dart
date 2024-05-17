9d85b308ad2705dd98f0fc6e23e9c208c4f99b1c
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-05-18 00:28:32 +0300

    Refactor imports and add debug prints
    This commit refactors imports in multiple files and adds debug prints to the `UserDataCubit` and `SimpleBlocObserver` classes for better debugging.

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -5,7 +5,6 @@ import 'package:chatie/core/app_logo.dart';
 import 'package:chatie/features/auth/presentation/views/widgets/forgot_password.dart';
 import 'package:chatie/features/auth/presentation/views/widgets/text_fields.dart';
 import 'package:chatie/features/chats/data/cubits/fecth_chats_cubit/fetch_chats_cubit.dart';
-import 'package:chatie/features/home/data/cubits/user_data_cubit/user_data_cubit.dart';
 import 'package:chatie/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 
 class SimpleBlocObserver implements BlocObserver {
   @override
@@ -20,16 +19,16 @@ class SimpleBlocObserver implements BlocObserver {
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
+    debugPrint("onError $error");
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
-    // TODO: implement onEvent
+    debugPrint("onEvent $event");
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    // TODO: implement onTransition
+    debugPrint("onTransition $transition");
   }
 }
